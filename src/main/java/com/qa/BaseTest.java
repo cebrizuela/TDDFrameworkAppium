d7b0ee73002fc4e3ed3e35aa05019eea7e6c8d8a
cebrizuela2020@gmail.com <cebrizuela2020@gmail.com>
2022-05-09 12:29:17 -0300

    Creando proyecto

---
+++ src/main/java/com/qa/BaseTest.java
@@ -0,0 +1,115 @@
+package com.qa;
+
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.qa.utils.TestUtils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.functions.ExpectedCondition;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+
+
+// En esta clase se inicializara el driver 
+public class BaseTest {
+  
+	protected AppiumDriver driver;
+	protected Properties props;
+	InputStream inputStream; // se usa para cuando el fichero donde esta la property no se encuentra en el mismo paquete que la clase que lo llama
+
+	
+	public void BaseTest() {
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+	
+  @org.testng.annotations.Parameters({"platformName","udid","deviceName", "avd"})
+  @BeforeTest
+  public void beforeTest(String platformName, String udid, String deviceName, String avd) throws Exception { 
+	   try {
+		   props = new Properties();
+		   
+		   // Codigo para cuando el fichero donde esta la property no se encuentra en el mismo paquete que la clase que lo llama
+		   String propFileName = "config.properties"; // nombre del fichero donde esta las propiedades
+		   inputStream =getClass().getClassLoader().getResourceAsStream(propFileName);
+		   
+		   //Este codigo permite cargar las propiedades en la variable props
+		   props.load(inputStream);
+		   
+		   
+		   
+		   DesiredCapabilities desiredCapability = new DesiredCapabilities();
+		   desiredCapability.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+		   desiredCapability.setCapability(MobileCapabilityType.AUTOMATION_NAME, props.getProperty("androidAutomationName"));
+		   desiredCapability.setCapability(MobileCapabilityType.UDID, udid);
+
+           //Para iniciar el emulador automaticamente
+		   desiredCapability.setCapability("avd",avd );
+		   desiredCapability.setCapability("avdLauchTimeout", 118000);
+
+
+           // Para instalar la App
+		   // Para generar la url de la app
+		   URL appUrl = getClass().getClassLoader().getResource(props.getProperty("androidAppLocation"));
+		   System.out.println(appUrl);
+           desiredCapability.setCapability(MobileCapabilityType.APP, appUrl);
+
+
+            // Para abrir la app usamos la opcion appPackage de appActivity
+           desiredCapability.setCapability("appPackage", props.getProperty("androidAppPackage"));
+           desiredCapability.setCapability("appActivity", props.getProperty("androidAppActivity"));
+           
+           URL url = new URL(props.getProperty("appiumURL"));
+           
+           driver = new AndroidDriver(url, desiredCapability);
+           String sessionId = driver.getSessionId().toString();
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+  }
+
+  public void waitForVisibility(MobileElement e) {
+  WebDriverWait wait = new WebDriverWait(driver, TestUtils.Wait);
+  wait.until(ExpectedConditions.invisibilityOf(e));
+  }
+  
+  public void click(MobileElement e) {
+	  waitForVisibility(e);
+	  e.click();
+  }
+  
+  public void sendKeys(MobileElement e, String txt) {
+	  waitForVisibility(e);
+	  e.sendKeys(txt);
+  }
+  
+  public void getAtTribute(MobileElement e, String attribute) {
+	  waitForVisibility(e);
+	  e.getAttribute(attribute);
+  }
+  
+  
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
